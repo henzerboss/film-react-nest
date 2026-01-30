66a7ea2386e94871fe70ad76cb6a54f977e366a4
Evgeny Evsyukov <henzerboss@gmail.com>
2026-01-30 18:18:11 +0100

    fix: git tests

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const port = configService.get<number>('PORT') || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
